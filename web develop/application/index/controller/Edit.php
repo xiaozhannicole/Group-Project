59e9e55fd9d7a64cc65ae2bb4323b5eb851968d1
XIAOYUE PENG <k1896845@kcl.ac.uk>
2019-03-13 17:37:16 +0000

    Add files via upload

--- web develop/application/index/controller/Edit.php
+++ web develop/application/index/controller/Edit.php
@@ -3,20 +3,37 @@ namespace app\index\controller;
 
 use think\controller;
 use think\Db;
-use app\index\model\Usr_tb;
+use app\index\model\File_tb;
+use think\request;
+use think\validate;
 //use think\Debug;
 
-class edit extends controller
+class Edit extends Controller
 {
+	public function index(){
+		$filename= $_GET['filename'];
+		$res = Db::table('file_tb')->where('file_name',$filename)->find();
 
-    public function index()
-    {
-    	return $this->fetch('edit',[
-    		'creator' => 'lala',
-    		'filename' => 'file1',
-    		'lastupdate' => '1day'
+		return $this->fetch('edit',[
+			'filename' => $filename,
+			'content'=>$res['content'],
+    		'lastupdate' => $res['update_time'],
+            'creator' => $res['create_usr'],
+            'file_id' => $res['file_id']
     	]);
+	}
 
-    	dump(Db::connect());
-    }
+	public function update(Request $request){
+		$post=$request->param();
+		if ($post){
+			Db::table('file_tb')->where('file_id',$post['file_id'])->update([
+				'content'=>$post['content_update'],
+				'update_usr'=>session('username')
+			]);
+			return $this->success('success');
+		}else{
+			return $this->error('failed');
+		}
+	}
+    
 }
